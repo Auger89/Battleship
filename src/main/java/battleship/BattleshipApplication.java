49530417935027d2628aebfc7b2c0fa2e375587e
Auger <auger89@gmail.com>
2017-04-28 16:41:53 +0200

    Finished Task 1. RESTful web service with Spring working.

--- src/main/java/battleship/BattleshipApplication.java
+++ src/main/java/battleship/BattleshipApplication.java
@@ -2,6 +2,8 @@ package battleship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BattleshipApplication {
@@ -9,4 +11,16 @@ public class BattleshipApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BattleshipApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository repository) {
+		return (args) -> {
+			// Save some players
+			repository.save(new Player("j.bauer@ctu.gov"));
+			repository.save(new Player("c.obrian@ctu.gov"));
+			repository.save(new Player("kim_bauer@gmail.com"));
+			repository.save(new Player("t.almeida@ctu.gov"));
+		};
+	}
+
 }
